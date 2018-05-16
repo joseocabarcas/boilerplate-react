65a1521954828fb01239376ce16f337e36f10e66
Jose Cabarcas <jcabarcas@joonik.com>
2018-05-16 07:22:27 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { React, Component } from "react";
+
+class IndexComponent extends Component {
+	constructor (args) {
+		// code
+		const jola = "";
+	}
+
+	// methods
+	render () {
+		return (
+			<div>
+			Hola mundo
+			</div>
+		);
+	}
+}
+
+export default IndexComponent;

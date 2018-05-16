fea22318eb6401a0e5f48695503faefc35068ead
Jose Cabarcas <jcabarcas@joonik.com>
2018-05-16 09:49:33 -0500

    second test route

--- src/router.jsx
+++ src/router.jsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Index from './ui/todo/containers/index';
+import Second from './ui/todo/containers/second';
 
 class Router extends Component {
     render () {
-    	const ad = 'hola';
         return (
             <Switch>
-                <Route exact path="/" component={Index}/>
+                <Route exact path="/" component={Index} />
+                <Route exact path="/second" component={Second} />
             </Switch>
         )
     }
---
+++ src/ui/todo/containers/second.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Second extends Component {
+	constructor (props) {
+		super(props);
+	}
+
+	render () {
+		return (
+			<div>
+			Second
+			</div>
+		)
+	}
+}
+
+export default Second;
